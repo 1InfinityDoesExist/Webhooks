d56802ee40c6aa5b434cbf95021320398a566295
1InfinityDoesExist <patelavinash508@gmail.com>
2022-04-05 13:10:27 +0545

    twitter crc verification code added

--- src/main/java/com/social/engagement/controller/WebhookVerification.java
+++ src/main/java/com/social/engagement/controller/WebhookVerification.java
@@ -1,23 +1,51 @@
 package com.social.engagement.controller;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.social.engagement.utils.InstagramConstants;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
 public class WebhookVerification {
+
+	private final String TWITTER_CONSUMER_SECRET = "xaaQw3Gg15eZQjgbTY0B2gTrCg2e7Pcx9YxoJTsbGHtubGZhOv";
+
+	@GetMapping("/twitter/callback/webhooks")
+	public ResponseEntity<?> crcTwitter(@RequestParam(value = "crc_token") String crcToken) {
+
+		String hash = null;
+
+		log.info("----CRC Token from Twitter : {}", crcToken);
+		try {
+			Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+			SecretKeySpec secret_key = new SecretKeySpec(TWITTER_CONSUMER_SECRET.getBytes(), "HmacSHA256");
+			sha256_HMAC.init(secret_key);
+			hash = Base64.encodeBase64String(sha256_HMAC.doFinal(crcToken.getBytes()));
+			log.info("----Hash Code that will be validated by twitter : {}", hash);
+
+		} catch (Exception e) {
+			throw new RuntimeException("----Use agaian exception and throw proper error msg.");
+		}
+
+		return ResponseEntity.status(HttpStatus.OK).body(new ModelMap().addAttribute("response_token", hash));
+	}
 
 	@GetMapping("/callback/webhooks")
 	public ResponseEntity<?> registeringCallbackUrlForSubscription(@RequestParam(value = "hub.mode") String mode,
@@ -40,9 +68,15 @@ public class WebhookVerification {
 	public void facebookDataStreamPage(HttpServletRequest request, String data) throws Exception {
 		String pushedJsonAsString = IOUtils.toString(request.getInputStream(), "utf-8");
 		log.info(" Event response : {}", pushedJsonAsString);
-		log.info("------Data : {}", data);
 
 		JSONObject entries = (JSONObject) new JSONParser().parse(pushedJsonAsString);
+		String object = (String) entries.get(InstagramConstants.OBJECT);
+		log.info("----Object : {}", object);
+		if (InstagramConstants.INSTAGRAM.equalsIgnoreCase(object)) {
+			log.info("----Object is instagram object.-----");
+
+		}
+
 		log.info("------Event Response to be sent to datalake. : {}", entries);
 
 	}
---
+++ src/main/java/com/social/engagement/utils/InstagramConstants.java
@@ -0,0 +1,8 @@
+package com.social.engagement.utils;
+
+public class InstagramConstants {
+
+	public static final String OBJECT = "object";
+	public static final String INSTAGRAM = "instagram";
+
+}
